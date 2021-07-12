ba4f79ea25ad94a1512d12374846f8c5a589305e
kandari012 <73176498+kandari012@users.noreply.github.com>
2021-07-12 17:46:18 +0530

    aprt 2 folder structure

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const expressLayout = require("express-ejs-layouts");
+const db = require("./config/mongoose");
 
-app.use("/",require("./routes"))
+app.use(expressLayout);
+app.use(express.static("./assets"));
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+app.use("/", require("./routes"));
 
-app.set("view engine","ejs");
-app.set("views","./views")
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.listen(port, function (err) {
   if (err) {
